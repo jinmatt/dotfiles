7e77a81a7325137f9a4f1e9e643f445d4b7917c2
Jinsu Mathew <jinsu@overflow.co>
2026-02-10 15:39:21 -0600

    feat: replace quickfix with Telescope for LSP navigation
    Replace default LSP quickfix window with Telescope pickers for better UX:
    - gd: Telescope lsp_definitions
    - gr: Telescope lsp_references
    - gi: Telescope lsp_implementations
    - gt: Telescope lsp_type_definitions
    
    Overrides NvChad's default LSP mappings via LspAttach autocmd.
    Telescope displays use filename_first for better visibility in nested paths.

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -3,4 +3,33 @@ require("nvchad.configs.lspconfig").defaults()
 local servers = { "html", "cssls", "ts_ls" }
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers 
+-- read :h vim.lsp.config for changing options of lsp servers
+
+-- Override LSP mappings to use Telescope instead of quickfix
+local map = vim.keymap.set
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local bufnr = args.buf
+    local opts = function(desc)
+      return { buffer = bufnr, desc = "LSP " .. desc }
+    end
+
+    -- Override NvChad's default mappings with Telescope
+    map("n", "gd", function()
+      require("telescope.builtin").lsp_definitions()
+    end, opts "Go to definition")
+
+    map("n", "gr", function()
+      require("telescope.builtin").lsp_references()
+    end, opts "Go to references")
+
+    map("n", "gi", function()
+      require("telescope.builtin").lsp_implementations()
+    end, opts "Go to implementation")
+
+    map("n", "gt", function()
+      require("telescope.builtin").lsp_type_definitions()
+    end, opts "Go to type definition")
+  end,
+}) 
