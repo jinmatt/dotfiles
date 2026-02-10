1c2a1b1f342d415466417b21b51b08faeba045a6
Jinsu Mathew <jinsu@overflow.co>
2026-02-10 15:47:39 -0600

    feat: add scrolloff setting
    Keep 8 lines visible above/below cursor when scrolling.

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -2,5 +2,8 @@ require "nvchad.options"
 
 -- add yours here!
 
+-- Keep 8 lines visible above/below cursor
+vim.opt.scrolloff = 8
+
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
