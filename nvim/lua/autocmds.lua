4084cc17c740bc1dfdd599cd215e09c445388f11
Jinsu Mathew <jinsu@overflow.co>
2026-02-13 10:57:29 -0600

    add auto-reload buffers on focus for external file changes

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -1,1 +1,7 @@
 require "nvchad.autocmds"
+
+-- Auto-reload buffers when files change on disk (e.g., external editor, coding agent)
+vim.o.autoread = true
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold" }, {
+  command = "silent! checktime",
+})
