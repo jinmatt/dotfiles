402cc9233a734cfbfea6f374ce2829702e146bb0
Jinsu Mathew <jinsu@overflow.co>
2026-02-13 11:09:17 -0600

    show relative path in statusline and increase tabufline width

--- nvim/lua/chadrc.lua
+++ nvim/lua/chadrc.lua
@@ -15,10 +15,34 @@ M.base46 = {
 }
 
 M.nvdash = { load_on_startup = true }
--- M.ui = {
---       tabufline = {
---          lazyload = false
---      }
--- }
+
+M.ui = {
+	tabufline = {
+		bufwidth = 30,
+	},
+
+	statusline = {
+		modules = {
+			file = function()
+				local stbufnr = function()
+					return vim.api.nvim_win_get_buf(vim.g.statusline_winid or 0)
+				end
+				local icon = "󰈚"
+				local path = vim.api.nvim_buf_get_name(stbufnr())
+				local name = (path == "" and "Empty") or vim.fn.fnamemodify(path, ":.")
+
+				if name ~= "Empty" then
+					local devicons_present, devicons = pcall(require, "nvim-web-devicons")
+					if devicons_present then
+						local ft_icon = devicons.get_icon(name)
+						icon = ft_icon or icon
+					end
+				end
+
+				return "%#St_file# " .. icon .. " " .. name .. " %#St_file_sep#" .. ""
+			end,
+		},
+	},
+}
 
 return M
